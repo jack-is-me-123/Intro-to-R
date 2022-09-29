38fca5d3f66005120db1528672a94e1b929d656d
Marks <Jack.Marks@dhsc.gov.uk>
2022-09-29 11:17:35 +0100

    September course updates

--- functions/reading_flag.R
+++ functions/reading_flag.R
@@ -8,7 +8,6 @@
 #'
 #' @examples reading_flag(math_score = 80, english_score = 60, history_score = 40)
 reading_flag <- function(math_score, english_score, history_score) {
-  case_when(math_score >= 1.5*english_score & 
-              math_score >= 1.5* history_score ~ "FLAG",
-            TRUE ~ "no flag")
+  if_else(math_score >= 1.5*english_score & 
+            math_score >= 1.5* history_score, "FLAG", "no flag")
 }
